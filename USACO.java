d08143d9c7174531925e8e520e269b69dd5f1360
Taejoon Kim <tkim00@stuy.edu>
2019-03-07 11:06:39 -0500

    bronze still in progress

--- USACO.java
+++ USACO.java
@@ -1,11 +1,29 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO {
   private int[][] pasture;
+  private String[] input;
 
-  public static int bronze(String filename) {
-
+  public static void main(String[] args) {
+    File problem = new File()
   }
 
-  public static int silver(String filename) {
+  // public static int bronze(String filename) {
+  //   int row = 0;
+  //   int col = 0;
+  //   int elevation = 0;
+  //   int instructions = 0;
+  //   File problem = new File(filename);
+  //   Scanner inf = new Scanner(problem);
+  //   int i = 0;
+  //   while (inf.hasNextLine()) {
+  //
+  //     i++
+  //   }
+  // }
 
-  }
+  // public static int silver(String filename) {
+  //
+  // }
 }
